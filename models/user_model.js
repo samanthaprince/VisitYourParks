b1a7fff08b30c7ba2f83ee66ea9f23c6c7b05bb0
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-28 11:08:57 -0700

    initial user model

---
+++ models/user_model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+  fullName:   String,
+  email:      {type: String, requied: true, unique: true},
+  password:   {type: String, required: true},
+  list
+});
