4e6577cc0b8a22acf100dd1dda70414d9babac15
Mikleane Chouinard <mikleane@msn.com>
2016-03-31 15:35:54 -0700

    geolocation user route

--- routes/geolocation_routes.js
+++ routes/geolocation_routes.js
@@ -2,7 +2,7 @@
 
 let parser = require('body-parser');
 let Parks = require(__dirname + '/../models/park_model');
-
+let User = require(__dirname + '/../models/user_model');
 let jwtAuth = require(__dirname + '/../lib/jwt_auth');
 
 
@@ -20,4 +20,38 @@ module.exports = (router) => {
         res.json({data: parks});
       });
     });
+
+  router.route('/userGeo/:user/geolocation/')
+    .get(jwtAuth, (req, res) => {
+      var userParks;
+      var longitude = req.query.longitude;
+      var latitude = req.query.latitude;
+      console.log('hit GET user id geolocation route');
+      if (req.params.user || req.user.admin) {
+        Parks.find({geometry:{$near:{$geometry:{type:'Point',coordinates:[longitude, latitude]},$maxDistance:321869}}}, (err, parks) => {
+          if (err) {
+            console.log(err);
+          }
+          User.findById(req.params.user, (err, user) => {
+            userParks = user.list;
+            console.log('user ob length ', userParks.length);
+            if (err) {
+              console.log(err);
+            }
+            var parksInRange = [];
+            for(var i = 0; i < parks.length; i++) {
+              for(var j = 0; j < userParks.length; j++) {
+                console.log('parksinrange', parks[i]);
+                console.log(userParks[j]);
+                if (JSON.stringify(parks[i]._id) == JSON.stringify(userParks[j].item)) {
+                  parksInRange.push(parks[i]);
+                }
+              }
+            }
+            res.json(parksInRange);
+          });
+        });
+      }
+    }
+  );
 };
